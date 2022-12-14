98043d8388b442698e3eb95368b707d553df9e78
Pep <57294729+PepC84@users.noreply.github.com>
2022-12-13 23:02:10 -0800

    Update critamarttreemain.ino

--- critamarttreemain.ino
+++ critamarttreemain.ino
@@ -1,100 +1,92 @@
+
 // mp1 addresses = 
 /*  -- |pin on the circuit| |name of pin on hardware| |ID of the LED on arduino|
---  21  GPA0  0
---  22  GPA1  1
---  23  GPA2  2
---  24  GPA3  3
---  25  GPA4  4
---  26  GPA5  5
---  27  GPA6  6
---  28  GPA7  7
---  1   GPB0  8
---  2   GPB1  9
---  3   GPB2  10
---  4   GPB3  11
---  5   GPB4  12
---  6   GPB5  13
---  7   GPB6  14
---  8   GPB7  15
+--  21  GPA0  0  G3
+--  22  GPA1  1  G#3
+--  23  GPA2  2  A3
+--  24  GPA3  3  A#3
+--  25  GPA4  4  B3
+--  26  GPA5  5  C4
+--  27  GPA6  6  C#4
+--  28  GPA7  7  D4
+--  1   GPB0  8  D4#
+--  2   GPB1  9  E4
+--  3   GPB2  10 F4
+--  4   GPB3  11 F#4
+--  5   GPB4  12 G4
+--  6   GPB5  13 G#4
+--  7   GPB6  14 A4
+--  8   GPB7  15 A#4
 */
 //mp2 addresses = 
 /* substract 16 for actual address
---  21  GPA0  16
---  22  GPA1  17
---  23  GPA2  18
---  24  GPA3  19
---  25  GPA4  20
---  26  GPA5  21
---  27  GPA6  22
---  28  GPA7  23
---  1   GPB0  24
---  2   GPB1  25
---  3   GPB2  26
---  4   GPB3  27
---  5   GPB4  28
---  6   GPB5  29
+--  21  GPA0  16 B4
+--  22  GPA1  17 C5
+--  23  GPA2  18 C#5
+--  24  GPA3  19 D5
+--  25  GPA4  20 D#5
+--  26  GPA5  21 E5
+--  27  GPA6  22 F5
+--  28  GPA7  23 F#5
+--  1   GPB0  24 G5
+--  2   GPB1  25 G#5
+--  3   GPB2  26 A5
+--  4   GPB3  27 A#5
+--  5   GPB4  28 B5
+--  6   GPB5  29 B#5
 --  7   GPB6  30
 --  8   GPB7  31
 */
-short int ledStates [31];
-long ledTimers [31];
+// h,00,00,00,e ps send format
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7,8,9,10,11,12);
 
 #include <Adafruit_MCP23X17.h>
 #include <Wire.h>
 
 Adafruit_MCP23X17 mcp1;
 Adafruit_MCP23X17 mcp2;
+
+
+short int ledStates [31];
+long ledTimers [31];
 int funnyVariable = 0;
-long lastTime = millis();
-long lastTurnON = millis();
-long lastTurnOFF = millis();
- short ledSelected = 16;
- int  ledPattern = 3000;
- long lastLEDPattern = millis();
- long offTimerCheck = millis();
- long ledSelect = millis();
+long lastResetLCD = millis();
+short ledSelected = 16;
+int  ledPattern = 3000;;
+long offTimerCheck = millis();
+long ledSelect = millis();
+
+int value1 = 0;
+int value2 = 0;
+int value3 = 0;
+long  timeStampReceived = millis();
+long timeStampContact = millis();
+long timeStampLCD = millis();
+long timeStampClearLCD = millis();
+boolean timeSinceNaN = false;
+String inString; 
+int inByte;
+boolean clearedLCD = false;
+boolean toggleVals = false;
+boolean firstContact = false;
+String  value1String;
+String  value2String;
+String  value3String;
+
 void setup() {
-  Serial.begin(9600);
-  //while (!Serial);
-  Serial.println("MCP23xxx Blink Test!");
-mcp1.begin_I2C(0x20); //low low low
-mcp2.begin_I2C(0x21); //high high high
+Serial.begin(9600);
+mcp1.begin_I2C(0x21); //low low high
+mcp2.begin_I2C(0x22); //low high low
 
-    mcp1.pinMode(15, OUTPUT);
-mcp2.pinMode(15, OUTPUT);
-  mcp1.pinMode(14, OUTPUT);
-mcp2.pinMode(14, OUTPUT);
-  mcp1.pinMode(13, OUTPUT);
-mcp2.pinMode(13, OUTPUT);
-  mcp1.pinMode(12, OUTPUT);
-mcp2.pinMode(12, OUTPUT);
-  mcp1.pinMode(11, OUTPUT);
-mcp2.pinMode(11, OUTPUT);
-  mcp1.pinMode(10, OUTPUT);
-mcp2.pinMode(10, OUTPUT);
-  mcp1.pinMode(9, OUTPUT);
-mcp2.pinMode(9, OUTPUT);
-  mcp1.pinMode(8, OUTPUT);
-mcp2.pinMode(8, OUTPUT);
-  mcp1.pinMode(7, OUTPUT);
-mcp2.pinMode(7, OUTPUT);
-  mcp1.pinMode(6, OUTPUT);
-mcp2.pinMode(6, OUTPUT);
-  mcp1.pinMode(5, OUTPUT);
-mcp2.pinMode(5, OUTPUT);
-  mcp1.pinMode(4, OUTPUT);
-mcp2.pinMode(4, OUTPUT);
-  mcp1.pinMode(3, OUTPUT);
-mcp2.pinMode(3, OUTPUT);
-  mcp1.pinMode(2, OUTPUT);
-mcp2.pinMode(2, OUTPUT);
-  mcp1.pinMode(1, OUTPUT);
-mcp2.pinMode(1, OUTPUT);
-  mcp1.pinMode(0, OUTPUT);
-mcp2.pinMode(0, OUTPUT);
-
-  Serial.println("volas");
-}
+ for(int i=0;i<16;i++) {
+  mcp1.pinMode(i, OUTPUT);
+  mcp2.pinMode(i, OUTPUT);
+  if(i==15) {
+    break;
+  }
+ }
+} 
 
 void loop() {
   if(millis() - ledSelect > 20) {
